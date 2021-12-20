956f33f87fb487f988682f92b028f4207ead1509
Steve <stevej311@gmail.com>
2021-12-20 16:20:52 -0700

    Added builder role / run_creep role differentiation and role bodies dict

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import harvester
+import builder
 # defs is a package which claims to export all constants and some JavaScript objects, but in reality does
 #  nothing. This is useful mainly when using an editor like PyCharm, so that it 'knows' that things like Object, Creep,
 #  Game, etc. do exist.
@@ -16,18 +17,24 @@ __pragma__('noalias', 'set')
 __pragma__('noalias', 'type')
 __pragma__('noalias', 'update')
 
-roles = ["harvester"]
-role_counts = {"harvester": 15}
+roles = ["harvester", "builder"]
+role_counts = {"harvester": 15,
+               "builder": 3}
+role_bodies = {"harvester": [WORK, CARRY, MOVE, MOVE],
+               "builder": [WORK, WORK, MOVE, MOVE]}
+
 
 def main():
     """
     Main game logic loop.
     """
-
     # Run each creep
     for name in Object.keys(Game.creeps):
         creep = Game.creeps[name]
-        harvester.run_harvester(creep)
+        if creep.memory.role == "harvester":
+            harvester.run_harvester(creep)
+        elif creep.memory.role == "builder":
+            builder.run_builder(creep)
 
     # Run each spawn
     for name in Object.keys(Game.spawns):
@@ -39,7 +46,7 @@ def main():
                 # console.log(JSON.stringify(num_creeps))
                 # If there are no creeps, spawn a creep once energy is at 250 or more
                 if num_creeps < 0 and spawn.room.energyAvailable >= 250:
-                    spawn.spawnCreep([WORK, CARRY, MOVE, MOVE], "{}-{}".format(role, Game.time),
+                    spawn.spawnCreep(role_bodies[role], "{}-{}".format(role, Game.time),
                                      {'memory': {'role': role}})
                 # If there are less than role_count creeps but at least one, wait until all spawns and extensions are full
                 # before spawning.
@@ -49,7 +56,7 @@ def main():
                         spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE], "{}-{}".format(role, Game.time),
                                          {'memory': {'role': role}})
                     else:
-                        spawn.spawnCreep([WORK, CARRY, MOVE, MOVE], "{}-{}".format(role, Game.time),
+                        spawn.spawnCreep(role_bodies[role], "{}-{}".format(role, Game.time),
                                          {'memory': {'role': role}})
 
 
