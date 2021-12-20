dfb6c7b7b137445acee7657680b580d66342ef54
Steve <stevej311@gmail.com>
2021-12-20 14:15:40 -0700

    createCreep is deprecated changed to spawnCreep

--- src/main.py
+++ src/main.py
@@ -35,15 +35,15 @@ def main():
             num_creeps = _.sum(Game.creeps, lambda c: c.pos.roomName == spawn.pos.roomName)
             # If there are no creeps, spawn a creep once energy is at 250 or more
             if num_creeps < 0 and spawn.room.energyAvailable >= 250:
-                spawn.createCreep([WORK, CARRY, MOVE, MOVE])
-            # If there are less than 15 creeps but at least one, wait until all spawns and extensions are full before
-            # spawning.
+                spawn.spawnCreep([WORK, CARRY, MOVE, MOVE])
+            # If there are less than 15 creeps but at least one, wait until all spawns and extensions are full
+            # before spawning.
             elif num_creeps < 15 and spawn.room.energyAvailable >= spawn.room.energyCapacityAvailable:
                 # If we have more energy, spawn a bigger creep.
                 if spawn.room.energyCapacityAvailable >= 350:
-                    spawn.createCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE])
+                    spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE])
                 else:
-                    spawn.createCreep([WORK, CARRY, MOVE, MOVE])
+                    spawn.spawnCreep([WORK, CARRY, MOVE, MOVE])
 
 
 module.exports.loop = main
