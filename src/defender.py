9191f56ab21a37d103d5359dfbe7a6c22757065c
Steve <stevej311@gmail.com>
2022-01-08 11:41:15 -0700

    Added defender logic, needs work on returning to base after killing enemy creeps.

--- src/defender.py
+++ src/defender.py
@@ -10,37 +10,34 @@ __pragma__('noalias', 'type')
 __pragma__('noalias', 'update')
 
 
-def run_defender(creep):
-    if creep.memory.building and creep.store[RESOURCE_ENERGY] == 0:
-        creep.memory.building = False
-        creep.say('collect')
+class Defender:
+    def __init__(self, creep):
+        self.creep = creep
+        self.home
 
-    if not creep.memory.building and creep.store.getFreeCapacity() == 0:
-        creep.memory.building = True
-        creep.say('build')
+    def run_defender(self):
+        if self.creep.memory.attacking and len(self.creep.room.find(FIND_HOSTILE_CREEPS)) == 0:
+            console.log("1")
+            self.creep.memory.attacking = False
+            del self.creep.memory.target
+            home = self.creep.room.find(FIND_MY_SPAWNS)
+            self.creep.memory.home = home.id
+        elif self.creep.memory.attacking and self.creep.memory.target:
+            console.log("2")
+            target = Game.getObjectById(self.creep.memory.target)
+            if self.creep.attack(target) == ERR_NOT_IN_RANGE:
+                self.creep.moveTo(target, {"visualizePathStyle": {"stroke": '#7bf600'}})
+        elif not self.creep.memory.attacking and self.creep.memory.home:
+            console.log("3")
+            home = Game.getObjectById(self.creep.memory.target)
+            self.creep.moveTo(home, {"visualizePathStyle": {"stroke": '#7bf600'}})
+        else:
+            console.log("4")
+            if len(self.creep.room.find(FIND_HOSTILE_CREEPS)) > 0:
+                target = self.creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
+                self.creep.memory.target = target.id
+                self.creep.memory.attacking = True
 
-    if creep.memory.building:
-        nearest = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
-        if creep.build(nearest) == ERR_NOT_IN_RANGE:
-            creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ffffff'}})
-    else:
-        # console.log("else clause")
-        # creep.memory.building = True
-        nearest = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
 
-           # _.filter(creep.room.find(FIND_STRUCTURES),
-           #          lambda x: (x.structureType == STRUCTURE_CONTAINER or
-           #                     x.structureType == STRUCTURE_STORAGE or
-           #                     x.structureType == STRUCTURE_SPAWN) and
-           #                    x.store.getUsedCapacity() > 0))
-        #  print(creep, "builder", nearest)
-        try:
-            result = creep.withdraw(nearest, RESOURCE_ENERGY)
-        except:
-            creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ffaa00'}})
-
-
-        # if creep.withdraw(nearest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE:
-        #    creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ffaa00'}})
-        #    result = creep.withdraw(nearest, RESOURCE_ENERGY)
-        #    console.log(JSON.stringify(result))
+####
+#### Need to fix bug where it gets stuck going between rooms where the are no enemies and so it delets the target from memory.
