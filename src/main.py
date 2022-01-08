5f1e20fa3437efc5b976c3262684aa15c232df94
Steve <stevej311@gmail.com>
2022-01-07 18:56:34 -0700

    Fixed comparison for # creeps in the room from < to <=

--- src/main.py
+++ src/main.py
@@ -42,10 +42,11 @@ def main():
         if not spawn.spawning:
             for role in roles:
                 # Get the number of our creeps in the room.
-                num_creeps = _.sum(Game.creeps, lambda c: c.pos.roomName == spawn.pos.roomName and c.memory.role == role)
-                # console.log(JSON.stringify(num_creeps))
+                num_creeps = _.sum(Game.creeps,
+                                   lambda c: c.pos.roomName == spawn.pos.roomName and c.memory.role == role)
                 # If there are no creeps, spawn a creep once energy is at 250 or more
-                if num_creeps < 0 and spawn.room.energyAvailable >= 250:
+                if num_creeps <= 0 and spawn.room.energyAvailable >= 250:
+                    console.log("here")
                     spawn.spawnCreep(role_bodies[role], "{}-{}".format(role, Game.time),
                                      {'memory': {'role': role}})
                 # If there are less than role_count creeps but at least one, wait until all spawns and extensions are full
