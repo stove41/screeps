01f691a6cb281bcd4126a0d9e33ebe6692fb6ed0
Steve <stevej311@gmail.com>
2022-01-23 23:07:04 -0700

    Reworked repair order for structures. Added lower limit on amt of stored nrg before will pick up.

--- src/builder.py
+++ src/builder.py
@@ -17,18 +17,38 @@ class Builder():
     def run_builder(self):
         if self.creep.memory.building and self.creep.store[RESOURCE_ENERGY] == 0:
             self.creep.memory.building = False
-            self.creep.say('collect')
 
         if not self.creep.memory.building and self.creep.store.getFreeCapacity() == 0:
             self.creep.memory.building = True
-            self.creep.say('build')
 
         if self.creep.memory.building:
-            nearest = self.creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES)
-            if self.creep.build(nearest) == ERR_NOT_IN_RANGE:
-                self.creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ff80ed'}})
+            nearest = self.creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES)
+            if nearest:
+                if self.creep.build(nearest) == ERR_NOT_IN_RANGE:
+                    self.creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ff80ed'}})
+            else:
+                nearest = self.creep.pos.findClosestByRange(FIND_MY_STRUCTURES,
+                                                            {"filter": lambda s:
+                                                             ((s.structureType == STRUCTURE_WALL and s.hits <= 50000)
+                                                              or (s.structureType == STRUCTURE_RAMPART and s.hits <= 50000)
+                                                              or s.structureType == STRUCTURE_ROAD
+                                                              and s.hits < s.hitsMax)})
+                if nearest:
+                    if self.creep.repair(nearest) == ERR_NOT_IN_RANGE:
+                        self.creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ff80ed'}})
+                else:
+                    nearest = self.creep.pos.findClosestByRange(FIND_STRUCTURES,
+                                                                {"filter": lambda s:
+                                                                 (s.structureType == STRUCTURE_CONTAINER
+                                                                  and s.hits < s.hitsMax)})
+                    if self.creep.repair(nearest) == ERR_NOT_IN_RANGE:
+                        self.creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ff80ed'}})
+
         else:
-            nearest = self.creep.pos.findClosestByRange(FIND_MY_SPAWNS)
+            nearest = self.creep.pos.findClosestByRange(FIND_MY_STRUCTURES,
+                                                        {"filter": lambda s:
+                                                         (s.structureType == STRUCTURE_EXTENSION and s.energy >= 50)
+                                                         or (s.structureType == STRUCTURE_SPAWN and s.energy > 50)})
             if self.creep.withdraw(nearest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE:
                 self.creep.moveTo(nearest, {"visualizePathStyle": {"stroke": '#ff80ed'}})
                 result = self.creep.withdraw(nearest, RESOURCE_ENERGY)
